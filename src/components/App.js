e3aa94e29baf597473b24de7790482c90e9a2929
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-02-05 14:04:45 +0530

    Update App.js
    updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Tic-Tac-Toe</h1>
+      <h2 data-ns-test="project-description">How to implement a 2-player Tic-Tac-Toe game using JavaScript? 
+
+It is quite easy to develop with some simple validations and error checks. Player-1 starts playing the game and both the players make their moves in consecutive turns. The player who makes a straight 3-block chain wins the game. This game is built on the front-end using simple logic and validation checks only.   
+
+Prerequisite: Basic knowledge of some front-end technologies like HTML, CSS, JavaScript. 
+
+</h2>
+
+    </div>
   )
 }
 
